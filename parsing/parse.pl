dde37296de18b326f335288c5015e5d5a9be5ce2
Het <hetyug04@gmail.com>
2025-12-10 21:28:04 -0500

    Final

--- parsing/parse.pl
+++ parsing/parse.pl
@@ -3,7 +3,29 @@
 % Your code here:
 %%%%%%%%%%%%%%%%%
 
-parse(X) :- ????
+parse(X) :- lines(X, []).
+
+lines(In, Out) :-
+    line(In, [';'|Rest]),    
+    lines(Rest, Out).
+lines(In, Out) :-
+    line(In, Out). 
+
+line(In, Out) :-
+    num(In, [','|Rest]),     
+    line(Rest, Out).         
+line(In, Out) :-
+    num(In, Out).            
+
+
+num(In, Out) :-
+    digit(In, Out).
+num(In, Out) :-
+    digit(In, Rest),
+    num(Rest, Out).
+
+digit([D|Rest], Rest) :-
+    member(D, ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']).
 
 % Example execution:
 % ?- parse(['3', '2', ',', '0', ';', '1', ',', '5', '6', '7', ';', '2']).
